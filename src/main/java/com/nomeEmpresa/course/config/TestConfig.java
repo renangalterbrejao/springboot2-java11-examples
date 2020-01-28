ed757de8ee1cd845843555d703bc33e916ea35a2
Renan Galter Brejao <renangalterbrejao@gmail.com>
2020-01-28 14:58:47 -0300

    Payment, one-to-one association

--- src/main/java/com/nomeEmpresa/course/config/TestConfig.java
+++ src/main/java/com/nomeEmpresa/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.nomeEmpresa.course.entities.Category;
 import com.nomeEmpresa.course.entities.Order;
 import com.nomeEmpresa.course.entities.OrderItem;
+import com.nomeEmpresa.course.entities.Payment;
 import com.nomeEmpresa.course.entities.Product;
 import com.nomeEmpresa.course.entities.User;
 import com.nomeEmpresa.course.entities.enums.OrderStatus;
@@ -78,6 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
